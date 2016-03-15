a807df26281e494501c8484b3aadaa283f0f0169
June Shaw <june.shaw@me.com>
2016-03-15 15:32:43 -0600

    initial class commit

---
+++ app/src/androidTest/java/com/example/homemariahome/androidappgroupproject_fe/Photo.java
@@ -0,0 +1,13 @@
+package com.example.homemariahome.androidappgroupproject_fe;
+
+/**
+ * Created by June on 3/15/16.
+ */
+public class Photo {
+    private int id;
+    private String description;
+    private int votes;
+    private int decisionId;
+    private boolean winner;
+}
+
