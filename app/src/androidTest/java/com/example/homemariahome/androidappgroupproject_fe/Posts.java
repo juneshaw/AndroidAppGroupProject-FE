55c4986416e34dc7290e5904674d537425e69507
June Shaw <june.shaw@me.com>
2016-03-15 23:44:46 -0600

    random post generator

--- app/src/androidTest/java/com/example/homemariahome/androidappgroupproject_fe/Posts.java
+++ app/src/androidTest/java/com/example/homemariahome/androidappgroupproject_fe/Posts.java
@@ -1,31 +1,44 @@
 package com.example.homemariahome.androidappgroupproject_fe;
+import java.util.Random;
+import java.util.ArrayList;
+//import java.net.HttpURLConnection;
+//import java.net.HttpURLConnection;
 
 /**
  * Created by June on 3/15/16.
  * Total of all posts of decisions.
  */
 public class Posts {
-    private Decision[] posts;
+    private Random randomGenerator;
+    private ArrayList<Decision> posts;
 
-    public Decision[] getPosts() {
-        return posts;
-    }
 
-    public void setPosts(Decision[] posts) {
+    public Posts(ArrayList<Decision> posts) {
         this.posts = posts;
     }
 
-    public Posts(Decision[] posts) {
+    public ArrayList<Decision> getPosts() {
+//        HttpURLConnection('')
+        return posts;
+
+    }
+
+    public void setPosts(ArrayList<Decision> posts) {
         this.posts = posts;
     }
 
     public void addPost(Decision post) {
-        this.posts.push(post);
+        this.posts.add(post);
+        // add this to database?
     }
 
-    public Decision Post() {
-        int length = this.posts.length;
-        int randomIndex = (int )(Math.random() * length + 1);
-        return posts[randomIndex];
+    public Decision getPost(int index) {
+        return this.posts.get(index);
+    }
+
+    public Decision randomPost() {
+        int index = randomGenerator.nextInt(this.posts.size());
+        return this.getPost(index);
+//        return decision;
     }
 }
